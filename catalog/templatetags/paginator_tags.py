239676098f6a68715e458e7eee79246fbced9c82
Antonio Cheong <windo.ac@gmail.com>
2022-06-11 20:11:32 +0800

    add custom template tag fix get_elided_page_range

---
+++ catalog/templatetags/paginator_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from django.core.paginator import Paginator
+
+register = template.Library()
+
+import sys
+
+@register.simple_tag
+def get_proper_elided_page_range(paginator, number, on_each_side=2, on_ends=1):
+    return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
